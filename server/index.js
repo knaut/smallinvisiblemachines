f33541cfbb116257a11686a99c60c42280d5d540
_knaut <info@knaut.net>
2018-07-14 22:54:16 -0700

    basic server running, biolerplate from cov react site

---
+++ server/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// IMPORTS
+import chalk from 'chalk';
+import clear from 'clear';
+
+// IMPORTS SERVER
+import Glue from 'glue';
+import Inert from 'inert';
+import Vision from 'vision';
+import Lout from 'lout';
+import Moment from 'moment';
+
+// CONFIG
+const manifest = {
+  server: {
+    port: process.env.PORT || 3000
+  },
+  register: {
+    plugins: [
+      // MODULES
+      Inert,
+      Vision,
+      Lout,
+
+      // STATIC ASSETS
+      // './routes/assets.js',
+
+      // STATIC PAGE ROUTES
+      // './routes/index.js'
+    ]
+  }
+}
+
+const options = {
+  relativeTo: __dirname
+};
+
+// SETUP
+const startServer = async function() {
+  try {
+    const server = await Glue.compose(manifest, options);
+    await server.start();
+
+    clear();
+    console.log(
+      chalk.bgWhite.bold.black(` SMALL INVISIBLE MACHINES `),
+      chalk.gray('server started at'),
+      chalk.green(`${server.info.protocol}://${server.info.address}:${server.info.port}`),
+      chalk.gray(`on`),
+      chalk.magenta(`${Moment().toISOString()}`)
+    );
+
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// RUN
+startServer();
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const SassPlugin = require('sass-webpack-plugin');
+const ProvidePlugin = require('webpack-provide-global-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/client.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin([
+      'dist'
+    ]),
+    new SassPlugin('src/styles/global.scss')
+  ],
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/assets/',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              plugins: function () { // post css plugins, can be exported to postcss.config.js
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
