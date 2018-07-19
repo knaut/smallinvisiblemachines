9e672de4d8d48844420f6143b1dc51b0f217b47e
_knaut <info@knaut.net>
2018-07-18 19:23:43 -0700

    implementing font awesome 5

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,28 @@
 'use strict';
 
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // COMPONENTS
+// ICONS
+import { 
+  faStroopwafel, 
+  faEnvelope, 
+  faPaperPlane
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  fab
+} from '@fortawesome/free-brands-svg-icons';
+
+// ICONS SETUP
+library.add(
+  fab,
+  faStroopwafel, 
+  faEnvelope, 
+  faPaperPlane
+);
 
 const Home = () => {
   return (
@@ -53,26 +73,28 @@ const Home = () => {
               <h2 className="text-uppercase text-muted">Daniel A. Anderson</h2>
               <hr/>
               <p className="lead">I'm a full-stack web developer with an eye for great design and a passion for crafting excellent user experiences.</p>
-              <p className="lead">I can bring your next web, app, or internet-of-things project come to life.</p>
+              <p className="lead">I can bring your next 
+                web, 
+                app, or internet-of-things project come to life.</p>
               <div className="row">
                 <div className="col">
                   <h5>
                     <a href="mailto:daniel@smallinvisiblemachines.com">
-                      Contact me by email  <i className="fa fa-envelope" />
+                      Contact me by email  <FontAwesomeIcon icon="envelope" />
                     </a>
                   </h5>
                 </div>
                 <div className="col">
                   <h5>
                     <a href="mailto:daniel@smallinvisiblemachines.com">
-                      Download my resume  <i className="fa fa-paper-plane" />
+                      Download my resume  <FontAwesomeIcon icon="paper-plane" />
                     </a>
                   </h5>
                 </div>
                 <div className="col">
                   <h5>
                     <a href="mailto:daniel@smallinvisiblemachines.com">
-                      View code samples  <i className="fa fa-github" />
+                      View code samples  <FontAwesomeIcon icon={["fab", "github"]} />
                     </a>
                   </h5>
                 </div>
@@ -85,7 +107,33 @@ const Home = () => {
 
       </div>
       <div className="container">
+        
+        <div className="row">
+          <div className="col-sm-12">
+            <h6 className="text-center text-uppercase text-muted">Services</h6>
+            <hr/>
+          </div>
+        </div>
 
+        <div className="row">
+          <div className="col-sm-4">
+            <h6 className="text-center text-uppercase text-info">End-to-end Javascript Development</h6>
+            
+            <p className="text-center text-muted"></p>
+            <p className="text-center text-muted">Start with a NodeJS foundation and build a single-page web 
+              app, mobile 
+              app, or custom CRM.</p>
+          </div>
+          <div className="col-sm-4">
+            <h6 className="text-center text-uppercase text-info">Pixel-perfect User Experience</h6>
+            <p className="text-center text-muted">Custom-tailored user interface design that is expressive yet simple.</p>
+          </div>
+          <div className="col-sm-4">
+            <h6 className="text-center text-uppercase text-info">Integrate The World of Things</h6>
+            <p className="text-center text-muted">Program your app to send a text message or get data from a sensor in real time.</p>
+          </div>
+        </div>
+      
       </div>
       
 
@@ -106,7 +154,9 @@ export default Home;
         <div className="row">
           <div className="col-sm-4">
             <h6 className="text-center text-uppercase text-muted">End-to-end Javascript Development</h6>
-            <p className="text-center text-uppercase text-muted">Start with a NodeJS foundation and build a single-page web app, mobile app, or custom CRM.</p>
+            <p className="text-center text-uppercase text-muted">Start with a NodeJS foundation and build a single-page web 
+              app, mobile 
+              app, or custom CRM.</p>
           </div>
           <div className="col-sm-4">
             <h6 className="text-center text-uppercase text-muted">Pixel-perfect User Experience</h6>
