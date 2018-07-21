4bd98413747ebc1e46bffe254a9f77e1dbe4e82d
_knaut <info@knaut.net>
2018-07-21 03:12:05 -0700

    spinner fixes

--- server/utils/renderHead.js
+++ server/utils/renderHead.js
@@ -33,9 +33,11 @@ export default function renderHead( html, preloadedState ) {
             z-index: -1;
           }
           .spinner {
-              -webkit-animation:spin 1s linear infinite;
-              -moz-animation:spin 1s linear infinite;
-              animation:spin 1s linear infinite;
+            height: 80px;
+            width: 80px;
+            -webkit-animation:spin 1s linear infinite;
+            -moz-animation:spin 1s linear infinite;
+            animation:spin 1s linear infinite;
           }
           @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
           @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
