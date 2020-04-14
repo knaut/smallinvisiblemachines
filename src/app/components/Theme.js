9f903c999a7e1892357815d36ddab73a69c33a62
knaut <daniel@smallinvisiblemachines.com>
2020-04-14 16:36:53 -0700

    added grommet with custom theme

---
+++ src/app/components/Theme.js
@@ -0,0 +1,72 @@
+// IMPORTS
+import React from 'react'
+
+// COMPONENTS
+import {
+	Grommet,
+	grommet as grommetTheme
+} from 'grommet'
+
+// UTILS
+import { deepMerge } from 'grommet/utils'
+
+const neutralLight = '#f2f2f2'
+const neutralGray = '#8d9aa6'
+const neutralDarkGray = '#455059'
+const neutralBlack = '#0d0d0d'
+const pastelRed = '#ffbdcd'
+const pastelYellow = '#ffedb8'
+const pastelGreen = '#b7edca'
+const pastelBlue = '#bbecf2'
+const pastelPurple = '#c8b8f5'
+const primaryRed = '#ff3c6e'
+const primaryYellow = '#ffce39'
+const primaryGreen = '#40ec7c'
+const primaryBlue = '#26c6da'
+const primaryPurple = '#8156f4'
+const darkRed = '#801f37'
+const darkYellow = '#80671c'
+const darkGreen = '#1e6e3a'
+const darkBlue = '#0f5159'
+const darkPurple = '#3d2975'
+
+export const simTheme = deepMerge(grommetTheme, {
+	global: {
+		// font: {
+		// 	family: 'Acumin, Helvetica Neue, Helvetica, Arial, sans-serif'
+		// }
+		colors: {
+			neutralLight,
+			neutralGray,
+			neutralDarkGray,
+			neutralBlack,
+			pastelRed,
+			pastelYellow,
+			pastelGreen,
+			pastelBlue,
+			pastelPurple,
+			primaryRed,
+			primaryYellow,
+			primaryGreen,
+			primaryBlue,
+			primaryPurple,
+			darkRed,
+			darkYellow,
+			darkGreen,
+			darkBlue,
+			darkPurple,
+		}
+	}
+})
+
+const SimTheme = ({ children }) => (
+	<Grommet
+		full
+		cssVars
+		theme={simTheme}
+	>
+		{children}
+	</Grommet>
+)
+
+export default SimTheme
--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+// COMPONENTS
+import Theme from './components/Theme'
+
 class App extends Component {
 	render() {
 		return (
-			<div>SIM</div>
+			<Theme>
+				<div>SIM</div>				
+			</Theme>
 		)
 	}
 }
