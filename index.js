1a43e90729bb4a3fa58366716338a873c941b23c
knaut <daniel@smallinvisiblemachines.com>
2020-05-04 15:09:45 -0700

    heroku process port

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const html = `
 
 const start = async () => {
 	const server = Hapi.server({
-		port: 3000,
-		host: 'localhost'
+		port: process.env.PORT || 3000,
+		host: '0.0.0.0'
 	});
 
 	await server.register(require('@hapi/inert'))
