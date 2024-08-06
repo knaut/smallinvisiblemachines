dd78e2637aa4c713164d83fbef7326763e2ce9d9
knaut <daniel@smallinvisiblemachines.com>
2024-08-05 19:00:03 -0700

    file updates

--- src/components/Resume/index.svelte
+++ src/components/Resume/index.svelte
@@ -16,7 +16,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-<div id="resume" >
+<div id="resume">
   <div id="resume-close" on:click={handleResumeClick}></div>
   <div id="resume-container">
     <article>
